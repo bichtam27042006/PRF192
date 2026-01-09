3ec4824f8aee3ec82db96eea3d24ba0fac795c7c
unknown <bichtam27042006@gmail.com>
2026-01-09 10:45:58 +0700

    add slot2 folder

---
+++ slot2/bai1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao 1 bien so nguyen
+    int a;
+    // Khoi tao gia tri bat ky
+    a = 27;
+    // In ra man hinh
+    printf("Gia tri cua a la: %d", a);
+    return 0;
+}
