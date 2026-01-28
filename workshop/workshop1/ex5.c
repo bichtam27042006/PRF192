da2b59b29bd374f6127b409b39337c7945a3ea24
Nguyễn Thị Bích Tâm <email_github@gmail.com>
2026-01-28 09:16:15 +0700

    workshop

--- workshop/workshop1/ex5.c
+++ workshop/workshop1/ex5.c
@@ -3,39 +3,34 @@
 //Từ 0.5km đến 30km: 15.000đ/km.
 //Từ km thứ 31 trở đi: 12.000đ/km.
 //Nếu thời gian chờ trên 5 phút, mỗi phút tiếp theo tính 1.000đ/phút.
-#include<stdio.h>
-int main(){
-	float km;
-	int t; 
-	float tien = 0;
-	printf("Nhap so km da di: ");
-	int kq = scanf("%f", &km );
-	if (kq!= 1 || km<0) {
-        printf("Nhap sai!\n");
+#include <stdio.h>
+int main() {
+    float soKm;
+    int soPhutCho;
+    printf("Nhap vao soKm: ");
+    int kq = scanf("%f", &soKm);
+    if (kq != 1 || soKm <= 0) {
+        printf("Nhap sai!");
         return 0;
     }
-    else {
-	printf(" Nhap thoi gian cho: ");
-	kq =scanf(" %d", &t);
-	if (kq!= 1 || t<0) {
-        printf("Nhap sai!\n");
+    printf("Nhap vao so phut cho: ");
+    kq = scanf("%d", &soPhutCho);
+    if (kq != 1 || soPhutCho < 0) {
+        printf("Nhap sai!");
         return 0;
     }
-	if(km<=0.5) {
-		tien = 12000;
-	}
-	else {
-		if(0.5<km && km<=30)
-			tien = 12000 + (km-0.5)*15000;
-		else 
-			tien = 12000 + 29.5*15000 + (km-30)*12000;
-	} 
-	if(t>5)
-		tien = tien + (t- 5)*1000;
-	printf("Tong tien taxi phai tra: %.f\n ",tien);
-	 printf("\n|  So km   | So phut cho |  Tien   |\n");
-    printf("|   %-6.1f |      %-7d| %-7.0f |\n", km, t, tien);
-}
-	return 0;
-	
+    float tongTien;
+    if (soKm < 0.5)
+        tongTien = 12000;
+    else if (soKm <= 30)
+        tongTien = 12000 + (soKm - 0.5) * 15000;
+    else
+        tongTien = 12000 + 29.5 * 15000 + (soKm - 30) * 12000;
+    if (soPhutCho > 5)
+        tongTien = tongTien + (soPhutCho - 5) * 1000; 
+        // ho?c tongTien += (soPhutCho - 5) * 1000;
+
+    printf("%-10s |%-20s |%-20s\n", "So km", "So Phut cho", "Tong Tien");
+    printf("%-10.1f |%-20d |%-20.0f", soKm, soPhutCho, tongTien);
+    return 0;
 }
