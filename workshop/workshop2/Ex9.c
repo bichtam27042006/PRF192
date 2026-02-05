69639b29f1d0d9dba7c837522171736c704f99d1
Nguyễn Thị Bích Tâm <email_github@gmail.com>
2026-02-05 23:35:30 +0700

    add

---
+++ workshop/workshop2/Ex9.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int random, doan;
+    srand(time(NULL));
+    random = rand()%100 + 1;
+    printf("Game doan so (1 - 100)\n");
+    printf("Nhap so ban doan: ");
+    while (1)
+    {
+        while (scanf("%d", &doan)!=1 || doan<=0 || doan>100)
+        {
+            printf("Nhap sai! Hay nhap so nguyen (1-100): ");
+            while (getchar() != '\n');
+        }
+
+        if (doan<random)
+        {
+            printf("Lon hon!\n");
+        }
+        else if (doan>random)
+        {
+            printf("Nho hon!\n");
+        }
+        else
+        {
+            printf("Chuc mung! Ban da doan dung\n");
+            break;
+        }
+    }
+    return 0;
+}
